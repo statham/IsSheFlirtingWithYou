90d0ab53bfce04821e0c4365a43f7a67ad8d893b
Ky Statham <kystatham@gmail.com>
2017-11-05 16:12:34 -0800

    splash activity and update main activity

--- app/src/main/java/com/example/kystatham/issheflirtingwithyou/activities/MainActivity.java
+++ app/src/main/java/com/example/kystatham/issheflirtingwithyou/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.kystatham.issheflirtingwithyou;
+package com.example.kystatham.issheflirtingwithyou.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.kystatham.issheflirtingwithyou.R;
 
 public class MainActivity extends AppCompatActivity {
 
